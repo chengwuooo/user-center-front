c24740744608923dcbeb211a1d141f5ca857fed6
chengwu <2477376240@qq.com>
2024-04-19 16:50:30 +0800

    修复bug

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -4,10 +4,12 @@
  */
 import {extend} from 'umi-request';
 import {message} from 'antd';
-import {stringify} from 'querystring';
 import {history} from "@@/core/history";
 
+const loginPath = '/user/login/';
+const RegisterPath = '/user/register/';
 
+const NO_NEED_LOGIN_WHITE_LIST = [loginPath, RegisterPath];
 
 /**
  * 配置request请求时的默认参数
@@ -44,25 +46,29 @@ request.interceptors.request.use((url, options): any => {
 request.interceptors.response.use(async (response): Promise<any> => {
   const res = await response.clone().json();
   if (process.env.NODE_ENV === 'development') {
-    console.log("res:::"+res);
-    console.log("JSON.stringify(res):::"+JSON.stringify(res));
-    console.log("res.code:::"+res.code);
-    console.log("response:::"+response);
-    console.log("res.data:::"+res.data);
+    console.log("res:::" + res);
+    console.log("JSON.stringify(res):::" + JSON.stringify(res));
+    console.log("res.code:::" + res.code);
+    console.log("response:::" + response);
+    console.log("res.data:::" + res.data);
   }
 
   if (res.code === 0) {
     return res.data;
   }
-  if (res.code === 40100) {
-    message.error('请先登录');
-    history.replace({
-      pathname: '/user/login',
-      search: stringify({
-        redirect: location.pathname,
-      }),
-    });
-  } else {
+  else if (res.code === 40100) {
+    if (!NO_NEED_LOGIN_WHITE_LIST.includes(history.location.pathname)) {
+      message.error('未登录');
+      // history.replace({
+      //   pathname: '/user/login',
+      //   search: stringify({
+      //     redirect: location.pathname,
+      //   }),
+      // });
+      history.push('/user/login');
+    }
+  }
+  else {
     message.error(res.description);
   }
   return res.data;
