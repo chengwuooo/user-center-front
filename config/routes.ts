b3e9977eb399f22d9c3305cb1f2f1ba032c7c447
chengwu <2477376240@qq.com>
2024-04-26 15:22:54 +0800

    移除多余的用户管理页

--- config/routes.ts
+++ config/routes.ts
@@ -16,8 +16,8 @@ export default [
     // component: './Admin',
     routes: [
       {path: '/admin/user-manage', name: '用户管理', icon: 'smile', component: './Admin/UserManage'},
-      {path: '/admin',name:'二级管理页', redirect: '/admin/sub-page'},
-      {path: '/admin/sub-page', component: './Admin'},
+      // {path: '/admin',name:'二级管理页', redirect: '/admin/sub-page'},
+      // {path: '/admin/sub-page', component: './Admin'},
     ],
   },
   {name:'查询表格',icon: 'table', path: '/list', component: './TableList'},
