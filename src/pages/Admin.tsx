5daca8f3d031d8d00971c9549b3b78ab1914d580
chengwu <2477376240@qq.com>
2024-04-26 15:18:30 +0800

    移除children

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,13 +1,11 @@
 import {PageContainer} from '@ant-design/pro-components';
 
 import React from 'react';
-const Admin: React.FC = (props) => {
-  // @ts-ignore
-  const { children } = props;
+const Admin: React.FC = () => {
   return (
     <PageContainer content={' 这个页面只有 admin 权限才能查看'}>
-      {children}
     </PageContainer>
   );
 };
 export default Admin;
+
